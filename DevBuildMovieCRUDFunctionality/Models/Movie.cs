63550dd12744aea2f01a6fafdb0f96d3df84b739
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-12-08 16:51:15 -0500

    Database funtions added

---
+++ DevBuildMovieCRUDFunctionality/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildMovieCRUDFunctionality.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; } = 0;
+
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(30, ErrorMessage = "Title must be a max of 30 Characters")]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        [Range(1880, 2021, ErrorMessage = "Year must be between 1880 and 2021")]
+        public int Year { get; set; }
+        public int RunTime { get; set; }
+    }
+}
---
+++ DevBuildMovieCRUDFunctionality/Models/MovieDAL.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildMovieCRUDFunctionality.Models
+{
+    public class MovieDAL
+    {
+        public void CreateMovie(Movie model)
+        {
+            
+
+            using(var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = $"insert into movies values" +
+                $"({model.ID}," +
+                $" {model.Title}," +
+                $" {model.Genre}," +
+                $" {model.Year}," +
+                $" {model.RunTime})";
+                connect.Open();
+                connect.Query<Movie>(sql);
+                connect.Close();
+            }
+
+        }
+
+        public List<Movie> GetMovies()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "SELECT * from movies";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+                return movies;
+            }
+        }
+
+        public Movie GetMovie(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = $"SELECT * from movies where id = {id}";
+                connect.Open();
+                Movie movies = connect.Query<Movie>(sql).First();
+                connect.Close();
+                return movies;
+            }
+        }
+
+
+        public void DeleteMovie(Movie model)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = $"delete from movies " +
+                    $"WHERE " +
+                $" id = {model.ID} AND" +
+                $" title = {model.Title} AND" +
+                $" genre = {model.Genre} AND" +
+                $" `year` = {model.Year} AND" +
+                $" runtime = {model.RunTime}";
+                connect.Open();
+                connect.Query<Movie>(sql);
+                connect.Close();
+            }
+
+        }
+
+        public void DeleteMovie(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = $"delete from movies " +
+                    $"WHERE " +
+                $" id = {id},";
+                connect.Open();
+                connect.Query<Movie>(sql);
+                connect.Close();
+            }
+
+        }
+
+    }
+}
